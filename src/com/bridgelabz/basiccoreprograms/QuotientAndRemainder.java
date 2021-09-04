63624c3f68a4efcf595b1604b50c07355f88c4c7
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 15:22:38 +0530

    ADDED: Finding quotient and remainder

---
+++ src/com/bridgelabz/basiccoreprograms/QuotientAndRemainder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class QuotientAndRemainder 
+{
+	public static void main(String args[]) 
+	{
+		int number=100,divisor=3;
+		System.out.println("Quotient: "+number/divisor+"\nRemainder: "+number%divisor);
+
+	}
+}
