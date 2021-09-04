f3df7203764dc207017993ba09bdcd03829b0054
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 14:12:40 +0530

    ADDED: Welcome Message

---
+++ src/com/bridgelabz/basiccoreprograms/FlipCoin.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class FlipCoin {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Basic Core Programs");
+
+	}
+
+}
