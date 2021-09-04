ab8bd9e861ecbd19ed468e378fa7e999edf60f8e
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 15:24:56 +0530

    ADDED: Checking if the number is even or odd

---
+++ src/com/bridgelabz/basiccoreprograms/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class EvenOrOdd {
+	public static void main(String args[]) 
+	{
+		int number=1213;
+		if(number%2==0)
+		{
+			System.out.println("The number "+number+" is a even number");
+		}
+		else
+		{
+			System.out.println("The number "+number+" is a odd number");
+		}
+			
+	}
+}
