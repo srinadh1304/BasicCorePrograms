f3ed89476bc8fdd606fbcc199a78570b7134c603
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 14:26:09 +0530

    ADDED: Flipping a coin and finding the heads and tails percentage

--- src/com/bridgelabz/basiccoreprograms/FlipCoin.java
+++ src/com/bridgelabz/basiccoreprograms/FlipCoin.java
@@ -3,8 +3,30 @@ package com.bridgelabz.basiccoreprograms;
 public class FlipCoin {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Basic Core Programs");
-
+		int noOfTimesToFilp=(int)(Math.floor(Math.random()*10));
+		if(noOfTimesToFilp<0)
+		{
+			System.out.println("The number of times to flip a coin should be positive");
+		}
+		else
+		{
+			int headOrTail=0;
+			int headsCount=0,tailsCount=0;
+			for(int noOfFlips=0;noOfFlips<noOfTimesToFilp;noOfFlips++)
+			{
+				headOrTail=(int)(Math.random());
+				if(headOrTail<0.5)
+				{
+					tailsCount+=1;
+				}
+				else
+				{
+					headsCount+=1;
+				}
+			}
+			System.out.println("Heads Percentage : "+((headsCount/noOfTimesToFilp)*100));
+			System.out.println("Tails Percentage : "+((tailsCount/noOfTimesToFilp)*100));
+		}
 	}
 
 }
